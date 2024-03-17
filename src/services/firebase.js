adc5827295cdc05d9b401e89dd6effb73666c569
Nagu066 <nagarajmoger11@gmail.com>
2024-03-17 18:53:09 +0530

    Splash, Login, firebaseAuth, GooglSignin, Home, AudioPLayer

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,3 +1,3 @@
-export const webClientID = '442317675236-fqohbbktp1uo3oerojfie0t799hslri1.apps.googleusercontent.com';
+export const webClientID = 'use Your  Client ID here';
 
-export const webClientSecret = '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'
+export const webClientSecret = 'use your client Secret here';
